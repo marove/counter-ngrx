f62d5b9723a1844d44704420e497db900b07c0a3
Manuel Romero <manuel.romero@innocv.com>
2020-03-17 13:09:05 +0100

    installation of store dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './counter/child/child.component';
 import { GrandchildComponent } from './counter/grandchild/grandchild.component';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { counterReducer } from './counter/counter.reducer';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { counterReducer } from './counter/counter.reducer';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({counter: counterReducer})
+    StoreModule.forRoot({counter: counterReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
