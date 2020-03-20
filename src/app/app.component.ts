992327d70b62fbfe47b428389b1cb693a8b55f1c
Manuel Romero <manuel.romero@innocv.com>
2020-03-20 14:33:37 +0100

    outsource interface AppState

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as actions from './counter/counter.actions';
-
-interface AppState {
-  counter: number
-}
+import { AppState } from './app.reducers';
 
 @Component({
   selector: 'app-root',
